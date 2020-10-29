d8d1b1a7306e663316872fe45ddd090f8946746b
xiaoyang521style <mailyzhao@163.com>
2020-10-29 16:40:41 +0800

    first commit

--- ZYMediator/Classes/ZYMediator.h
+++ ZYMediator/Classes/ZYMediator.h
@@ -10,7 +10,11 @@
 NS_ASSUME_NONNULL_BEGIN
 extern NSString * const kZYMediatorParamsKeySwiftTargetModuleName;
 @interface ZYMediator : NSObject
-+ (instancetype)sharedInstance;
+// 远程App调用入口
+- (id)performActionWithUrl:(NSURL *)url completion:(void(^)(NSDictionary *info))completion;
+// 本地组件调用入口
+- (id)performTarget:(NSString *)targetName action:(NSString *)actionName params:(NSDictionary *)params shouldCacheTarget:(BOOL)shouldCacheTarget;
+- (void)releaseCachedTargetWithTargetName:(NSString *)targetName;
 
 @end
 
